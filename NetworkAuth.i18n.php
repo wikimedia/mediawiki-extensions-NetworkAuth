70015a9d756269b90ed16d23eefe57557642cedc
siebrand <siebrand@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2008-01-07 13:52:04 +0000

    Localisation updates for extension messages from Betawiki (2008-01-07 12:00 CET)
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/NetworkAuth@29382 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- NetworkAuth.i18n.php
+++ NetworkAuth.i18n.php
@@ -13,6 +13,14 @@ $messages['en'] = array(
 	'networkauth-purltext' => 'NetworkAuth [$1@$2]',
 );
 
+/** Arabic (العربية)
+ * @author Meno25
+ */
+$messages['ar'] = array(
+	'networkauth-name'     => 'أوث الشبكة',
+	'networkauth-purltext' => 'أوث الشبكة [$1@$2]',
+);
+
 /** Dutch (Nederlands)
  * @author Siebrand
  */
