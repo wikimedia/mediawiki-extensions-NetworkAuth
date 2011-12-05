2888b3ff5bb8d0d459913ffe47e913d3ea0bc527
reedy <reedy@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2011-12-05 15:54:02 +0000

    Fix indenting/whitespace
    Fix bug 32808, with refactoring of patch by Olaf Lenz
    
    Invert some conditionals to remove empty blocks
    
    Fix register_globals issue
    
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/extensions/NetworkAuth@105184 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- NetworkAuth.php
+++ NetworkAuth.php
@@ -10,6 +10,7 @@ if (!defined('MEDIAWIKI')) {
 }
 
 $wgNetworkAuthUsers[] = array();
+$wgNetworkAuthUser = 'HelpdeskComputer';
 
 $wgExtensionCredits['other'][] = array(
 	'path'           => __FILE__,
@@ -24,29 +25,29 @@ $wgExtensionFunctions[] = 'efNetworkAuth_Setup';
 $wgExtensionMessagesFiles['NetworkAuth'] = dirname(__FILE__) . '/NetworkAuth.i18n.php';
 
 function efNetworkAuth_Setup() {
-        global $wgRequest;
+	global $wgRequest;
 
-		# $wgTitle isn't initalized yet - but we need to know where we are
-		$pageTitle = Title::newFromURL( $wgRequest->getVal('title') );
-		
-		if (is_object($pageTitle)) {
-			# Doesn't apply to Userlogin and Userlogout pages - that breaks stuff
-			if ($pageTitle->isSpecial('Userlogin') || $pageTitle->isSpecial('Userlogout')) {
-				# bail!
-				return true;
-			}
+	# $wgTitle isn't initalized yet - but we need to know where we are
+	$pageTitle = Title::newFromURL( $wgRequest->getVal('title') );
+
+	if (is_object($pageTitle)) {
+		# Doesn't apply to Userlogin and Userlogout pages - that breaks stuff
+		if ($pageTitle->isSpecial('Userlogin') || $pageTitle->isSpecial('Userlogout')) {
+			# bail!
+			return true;
 		}
-		
-		efNetworkAuth_Authenticate();
-		
-		return true;
+	}
+
+	efNetworkAuth_Authenticate();
+
+	return true;
 }
 
 function efNetworkAuth_checkForNetworkAuthUser() {
 	global $wgNetworkAuthUsers;
-	
+
 	$ip = wfGetIP();
-	
+
 	foreach ($wgNetworkAuthUsers as $networkAuthUser) {
 		if ( isset( $networkAuthUser['user'] ) ) {
 			if ( isset( $networkAuthUser['iprange'] ) ) {
@@ -54,7 +55,7 @@ function efNetworkAuth_checkForNetworkAuthUser() {
 					$ranges = $networkAuthUser['iprange'];
 				else
 					$ranges = explode("\n", $networkAuthUser['iprange']);
-					
+
 				$hex = IP::toHex( $ip );
 				foreach ( $ranges as $range ) {
 					$parsedRange = IP::parseRange( $range );
@@ -65,13 +66,13 @@ function efNetworkAuth_checkForNetworkAuthUser() {
 					}
 				}
 			} 
-			
+
 			if ( isset( $networkAuthUser['ippattern'] ) ) {
 				if ( is_array( $networkAuthUser['ippattern'] ) )
 					$patterns = $networkAuthUser['ippattern'];
 				else
 					$patterns = explode("\n", $networkAuthUser['ippattern']);
-					
+
 				foreach ( $patterns as $pattern ) {
 					if ( preg_match( $pattern,  $ip) ) {
 						global $wgNetworkAuthHost;
@@ -86,7 +87,7 @@ function efNetworkAuth_checkForNetworkAuthUser() {
 					$patterns = $networkAuthUser['hostpattern'];
 				else
 					$patterns = explode("\n", $networkAuthUser['hostpattern']);
-			
+
 				$host = gethostbyaddr( $ip );
 				foreach ( $patterns as $pattern ) {
 					if ( preg_match( $pattern,  $host) ) {
@@ -100,76 +101,66 @@ function efNetworkAuth_checkForNetworkAuthUser() {
 			# No user for range - useless.
 		}
 	}
-	
+
 	return '';
 }
 
 function efNetworkAuth_Authenticate() {
 	global $wgUser;
-	
-	$wgNetworkAuthUser = 'HelpdeskComputer';
-	
 	if (!$wgUser->isLoggedIn()) {
 		//echo 'Logged out: ' . $wgUser->getName();
-		
+
+		$u = null;
 		$networkAuthUser = efNetworkAuth_checkForNetworkAuthUser();
 		if ( $networkAuthUser != '' ) {
 			global $wgNetworkAuthUser;
 			$wgNetworkAuthUser = $networkAuthUser;
 
-			$u = User::newFromName( $wgNetworkAuthUser );			
+			$u = User::newFromName( $wgNetworkAuthUser );
 		}
-		
-		if( is_null( $u ) || !User::isUsableName( $u->getName() ) ) {
-			# Not cool.  Bad config
-		} else {
-			if ( 0 == $u->getID() ) {
-				# Not cool.  Bad username
-			} else {
+
+		if( is_null( $u ) && !User::isUsableName( $u->getName() ) ) {
+			if ( !$u->isAnon() ) {
 				# Finally.
 				$u->load();
 				$wgUser = $u;
-				
+
 				# Since we're not really logged in, just pretending - force a logout
 				# before the page gets displayed.
 				global $wgHooks;
 				$wgHooks['BeforePageDisplay'][] = 'efNetworkAuth_ForceLogout';
-				
+
 				# Add a display message to the personal URLs
 				$wgHooks['PersonalUrls'][] = 'efNetworkAuth_PersonalUrls';
 			}
 		}
-	} else {
-		# Already logged in, do nothing.
 	}
 	return true;
 }
 
 function ar_gethostbyaddr($ip) {
-  $output = `host -W 1 $ip`;
-  if (ereg('.*pointer ([A-Za-z0-9.-]+)\..*',$output,$regs)) {
-    return $regs[1]; 
-  }
-  return $ip;
+	$output = `host -W 1 $ip`;
+	if (ereg('.*pointer ([A-Za-z0-9.-]+)\..*',$output,$regs)) {
+		return $regs[1];
+	}
+	return $ip;
 } 
 
-function efNetworkAuth_PersonalUrls($personal_urls, $title) {
+function efNetworkAuth_PersonalUrls(&$personal_urls, &$title) {
 	global $wgNetworkAuthUser, $wgNetworkAuthHost;
-	
+
 	// Lazy load the i18n stuff here
-	
-	
+
 	if (isset($personal_urls['anonuserpage'])) {
 		$personal_urls['anonuserpage']['text'] = 
 			wfMsg('networkauth-purltext', $wgNetworkAuthUser, $wgNetworkAuthHost);
 	} else {
-		global $wgUser;
 		$newUrls['anonuserpage'] = array(
 			'text' => wfMsg('networkauth-purltext', $wgNetworkAuthUser, $wgNetworkAuthHost),
 			'href' => null,
 			'active' => true
 		);
-		
+
 		foreach($personal_urls as $key => $value) {
 			if ( $key == 'login' )
 				$newUrls['anonlogin'] = $value;
@@ -185,6 +176,6 @@ function efNetworkAuth_ForceLogout($out) {
 	# Force logout after most of the permission checks
 	global $wgUser;
 	$wgUser->logout();
-	
+
 	return true;
 }
